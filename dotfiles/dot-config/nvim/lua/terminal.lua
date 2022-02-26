6695ac3824cad7dfa3eee0896ab8713503c19749
sam3llis <samwilliamellis@gmail.com>
2022-02-26 00:14:24 +0000

    update: cleaner refactoring of floating-window and bug fixes

--- dotfiles/dot-config/nvim/lua/terminal.lua
+++ dotfiles/dot-config/nvim/lua/terminal.lua
@@ -1,72 +1,52 @@
--- NOTE(sam3llis):
 -- Add option to move back to original window after a terminal execution.
 -- Create a function to have different position options (TL, TR, ML, MR, BL, BR, MIDDLE, etc.)
 -- print messages = false | true
 -- use similar to <C-w>hjkl to move between specifically floating windows?
+--
+-- TODO:
+--
+-- Create a 'launch new terminal' command, that creates a fresh floating window in toggle terminal
+-- view, then splits the screen to show both terminals at the same time :)
+--
+-- Open REPL command, probably similar to run_active_buffer()
+--
+-- Customise location of windows on screen, e.g. for an iPython REPL, I think I'd prefer it being static
+-- on the RHS of the editor approx (90 characters) across.
+--
+-- Add :w <bar> into run_active_buffer() as a standard default (e.g. run_active_buffer(save = true)).
 
 if not vim.fn.has("nvim-0.5.0") then
-    return
+  return
 end
 
-local window = require('window')
+Terminal = {}
+Terminal.__index = Terminal
+setmetatable(Terminal, require('window'))
 
--- local terminal = {
---   buf = nil,
---   win = nil,
---   pid = nil,
--- }
 
-local terminal = {}
+function Terminal:open(cmd)
+  Window.open(self)
 
-terminal.open = function (cmd)
-  local buf = window.open()
-
-  if not window.buf then
+  if not self.pid then
     local cmd = cmd or string.format('%s --login', os.getenv('SHELL'))
-    window.pid = vim.fn.termopen(cmd)
+    self.pid = vim.fn.termopen(cmd)
   end
 
-  if vim.fn.has_key(vim.g.plugs, 'nvim-vim-termbinds') == 0 then -- NOTE: vim-plug only.
+  if not vim.fn.has_key(vim.g.plugs, 'nvim-vim-termbinds') then
     vim.cmd('startinsert')
   end
 
-  vim.cmd("autocmd! TermClose * lua require('terminal').exit()")
-
-  window.buf = buf
-end
-
-terminal.close = function (force)
-  local force = force or nil
-  window.close(force)
-end
-
-terminal.exit = function (force)
-  -- NOTE: enable terminal.exit force (not working)
-  if force then
-    if vim.api.nvim_buf_is_loaded(window.buf) then
-      vim.api.nvim_buf_delete(window.buf, { force = true })
-    end
-    vim.fn.jobstop(window.pid)
-  end
-
-  window.buf = nil
-  window.win = nil
-  window.pid = nil
-end
-
-terminal.toggle = function (cmd)
-  if not window.win then
-    terminal.open(cmd)
-  else
-    terminal.close()
-  end
-end
-
--- NOTE: only works with python for now - figure this out (maybe just a giant hashmap).
-terminal.run_active_buffer = function ()
-  -- local filetype = vim.bo.filetype
-  terminal.open('python3' .. ' ' .. vim.fn.expand('%'))
+  vim.cmd("autocmd TermClose <buffer> lua require('terminal'):close()")
 end
 
 
-return terminal
+function Terminal.run_active_buffer()
+  local filename = vim.fn.expand('%')
+  local filetype = vim.bo.filetype
+
+  t = Window.new():open()
+  vim.fn.termopen('python3' .. ' ' .. filename)
+end
+
+
+return Terminal
--- dotfiles/dot-config/nvim/lua/window.lua
+++ dotfiles/dot-config/nvim/lua/window.lua
@@ -1,12 +1,144 @@
-window = {
-  buf = nil,
-  win = nil,
-  pid = nil,
-  defs = {
-    winblend = 15,
-    percentage = 0.8,
-  },
-}
+if not vim.fn.has("nvim-0.5.0") then
+    return
+end
+
+Window = {}
+Window.__index = Window
+
+
+function Window.new(opts)
+  local instance = setmetatable({}, Window)
+
+  -- Initialise variables.
+  instance.buf  = -1
+  instance.win  = nil
+  instance.pid  = nil
+  instance.opts = opts
+
+  return instance
+end
+
+
+function Window:open()
+  local buf
+
+  if self.buf and vim.api.nvim_buf_is_loaded(self.buf) then
+    buf = self.buf
+  else
+    buf = vim.api.nvim_create_buf(false, true)
+  end
+
+  local opts = get_opts()
+  self.win = vim.api.nvim_open_win(buf, true, opts)
+  self.buf = buf
+
+end
+
+
+function Window:close(force)
+  if not self.win then
+    return
+  end
+
+  if vim.api.nvim_win_is_valid(self.win) then
+    vim.api.nvim_win_close(self.win, false)
+    self.win = nil
+  end
+
+  if force then
+    vim.fn.jobstop(self.pid)
+
+    if vim.api.nvim_buf_is_loaded(self.buf) then
+      vim.api.nvim_buf_delete(self.buf, { force = true })
+    end
+
+    self.buf = -1
+    self.win = nil
+    self.pid = nil
+  end
+end
+
+
+function Window:toggle(cmd)
+  if not self.win then
+    self:open(cmd)
+  else
+    self:close()
+  end
+end
+
+
+-- -- Create a new window instance.
+-- function Window:new (defs)
+--   local o = {
+--     buf = nil,
+--     win = nil,
+--     pid = nil,
+--     defs = defs,
+--   }
+
+--   return setmetatable(o, { __index = self })
+-- end
+
+
+-- function Window:open ()
+--   local buf = nil
+
+--   print('foo')
+--   print(w.defs)
+
+--   if self.buf and vim.api.nvim_buf_is_loaded(self.buf) then
+--     buf = self.buf
+--   else
+--     buf = vim.api.nvim_create_buf(false, true)
+--   end
+
+--   local opts = self.get_opts()
+
+--   self.win = vim.api.nvim_open_win(buf, true, opts)
+--   return buf
+-- end
+
+
+-- function Window:close (force)
+--   if not self.win then
+--     return
+--   end
+
+--   if vim.api.nvim_win_is_valid(self.win) then
+--     vim.api.nvim_win_close(self.win, false)
+--     self.win = nil
+--   end
+
+--   if force then
+--     if vim.api.nvim_buf_is_loaded(self.buf) then
+--       vim.api.nvim_buf_delete(self.buf, { force = true })
+--     end
+
+--     vim.fn.jobstop(self.pid)
+
+--     self.buf = nil
+--     self.pid = nil
+--   end
+-- end
+
+-- w = Window:new ({winblend=15, percentage=0.8})
+-- w:open()
+
+
+
+
+
+
+-- window = {
+--   buf = nil,
+--   win = nil,
+--   pid = nil,
+--   defs = {
+--     winblend = 15,
+--     percentage = 0.8,
+--   },
+-- }
 
 
 local function get_defaults(opts, defs)
@@ -26,8 +158,10 @@ local function get_defaults(opts, defs)
 end
 
 
-window.get_opts = function (opts)
-  local opts = get_defaults(opts, window.defs)
+-- window.get_opts = function (opts)
+function get_opts(opts)
+  -- local opts = get_defaults(opts, defs)
+  local opts = get_defaults(opts, {percentage=0.8, winblend=15})
 
   local width = math.floor(vim.o.columns * opts.percentage)
   local height = math.floor(vim.o.lines * opts.percentage)
@@ -58,43 +192,4 @@ window.get_opts = function (opts)
 end
 
 
-window.open = function ()
-  local buf = nil
-
-  if window.buf and vim.api.nvim_buf_is_loaded(window.buf) then
-    buf = window.buf
-  else
-    buf = vim.api.nvim_create_buf(false, true)
-  end
-
-  local opts = window.get_opts()
-
-  window.win = vim.api.nvim_open_win(buf, true, opts)
-  return buf
-end
-
-
-window.close = function(force)
-  if not window.win then
-    return
-  end
-
-  if vim.api.nvim_win_is_valid(window.win) then
-    vim.api.nvim_win_close(window.win, false)
-    window.win = nil
-  end
-
-  if force then
-    if vim.api.nvim_buf_is_loaded(window.buf) then
-      vim.api.nvim_buf_delete(window.buf, { force = true })
-    end
-
-    vim.fn.jobstop(window.pid)
-
-    window.buf = nil
-    window.pid = nil
-  end
-end
-
-
-return window
+return Window
