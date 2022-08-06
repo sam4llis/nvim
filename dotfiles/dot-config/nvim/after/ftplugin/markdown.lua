51659b75ec68ca445bd3ff581772c05d0034ad5c
Sam Ellis <samwilliamellis@gmail.com>
2022-08-06 14:28:29 +0100

    feat: conceal link destinations in markdown files
    Utilising [nvim-treesitter](https://github.com/nvim-treesitter/nvim-treesitter),
    all link destinations have been concealed in Normal mode to improve the
    readability of markdown documents. Insert mode makes these links
    editable via the markdown [text](destination) syntax.

--- dotfiles/dot-config/nvim/after/ftplugin/markdown.lua
+++
@@ -1,3 +0,0 @@
-vim.opt_local.spell = true       -- Enable spell-checking.
-vim.opt_local.colorcolumn = '80' -- Enable colorcolumn at row 80.
-vim.opt_local.textwidth = 80     -- Set a maximum width for inserted text.
---
+++ dotfiles/dot-config/nvim/after/ftplugin/markdown/settings.lua
@@ -0,0 +1,5 @@
+vim.opt_local.colorcolumn = '80'  -- Enable colorcolumn at row 80.
+vim.opt_local.concealcursor = 'n' -- Hide all concealed text in Normal mode.
+vim.opt_local.conceallevel = 2    -- Concealed text is completely hidden unless it has a custom replacement defined.
+vim.opt_local.spell = true        -- Enable spell-checking.
+vim.opt_local.textwidth = 80      -- Set a maximum width for inserted text.
