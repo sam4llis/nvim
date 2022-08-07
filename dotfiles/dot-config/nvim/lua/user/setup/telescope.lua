9d9b20fbe0d91bfec7bc17f02b05dd3366c22be9
Sam Ellis <samwilliamellis@gmail.com>
2022-08-07 19:49:45 +0100

    chore: fix formatting

--- dotfiles/dot-config/nvim/lua/user/setup/telescope.lua
+++ dotfiles/dot-config/nvim/lua/user/setup/telescope.lua
@@ -3,39 +3,32 @@ if vim.fn.has('nvim-0.7.0') ~= 1 then
   return
 end
 
-local actions = require("telescope.actions")
+local actions = require('telescope.actions')
 
 require('telescope').setup{
   defaults = {
     mappings = {
       i = {
-        ["<C-h>"] = "which_key",
-        ["<esc>"] = actions.close,
+        ['<esc>'] = actions.close,
       },
     },
-    prompt_prefix = "     ",
-    selection_caret = "  ",
-    entry_prefix = "  ",
-    sorting_strategy = "ascending",
+    prompt_prefix = '     ',
+    selection_caret = '  ',
+    entry_prefix = '  ',
+    sorting_strategy = 'ascending',
     layout_config = {
-            horizontal = {
-                prompt_position = "top",
-                preview_width = 0.55,
-                results_width = 0.8,
-            },
-             vertical = {
-                mirror = false,
-            },
-            width = 0.8,
-            height = 0.8,
-            preview_cutoff = 120,
-  },
-
-    -- layout_strategy = "vertical",
-    -- sorting_strategy = "ascending",
-    -- layout_config = {
-    --   prompt_position = 'top',
-    -- },
+      horizontal = {
+        prompt_position = 'top',
+        preview_width = 0.55,
+        results_width = 0.8,
+      },
+      vertical = {
+        mirror = false,
+      },
+      width = 0.8,
+      height = 0.8,
+      preview_cutoff = 120,
+    },
   },
   pickers = {},
   extensions = {
@@ -43,9 +36,9 @@ require('telescope').setup{
       fuzzy = true,
       override_generic_sorter = true,
       override_file_sorter = true,
-      case_mode = "smart_case",
+      case_mode = 'smart_case',
     },
   },
 }
 
--- require('telescope').load_extension('fzf')
+require('telescope').load_extension('fzf')
