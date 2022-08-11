3dae8f4a963b9f269655aa325fab74cf019e078d
Sam Ellis <samwilliamellis@gmail.com>
2022-08-12 00:17:44 +0100

    update: revamp `lsp.lua`
    Credit to [joechrisellis](https://github.com/joechrisellis) for the LSP
    section in his dotfiles!

--- dotfiles/dot-config/nvim/lua/user/setup/lsp.lua
+++ dotfiles/dot-config/nvim/lua/user/setup/lsp.lua
@@ -3,46 +3,143 @@ if vim.fn.has('nvim-0.7.0') ~= 1 then
   return
 end
 
-local on_attach = function(client, bufnr)
-  local opts = { noremap=true, silent=true }
-  normal_completion_prefix = 'yc'
-  visual_completion_prefix = '<leader>y'
+local lspconfig = require('lspconfig')
 
-  vim.api.nvim_buf_set_option(0, 'tagfunc', 'v:lua.vim.lsp.tagfunc')
-  vim.api.nvim_buf_set_option(0, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+base_custom_lsp_attach = function(client, bufnr)
+  vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+  vim.api.nvim_buf_set_option(bufnr, 'tagfunc', 'v:lua.vim.lsp.tagfunc')
 
-  vim.api.nvim_buf_set_keymap(0, 'n', '<c-]>', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(0, 'n', normal_completion_prefix .. 'a', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(0, 'n', normal_completion_prefix .. 'd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(0, 'n', normal_completion_prefix .. 'i', '<cmd>lua vim.lsp.buf.incoming_calls()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(0, 'n', normal_completion_prefix .. 'm', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(0, 'n', normal_completion_prefix .. 'n', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(0, 'n', normal_completion_prefix .. 'o', '<cmd>lua vim.lsp.buf.outgoing_calls()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(0, 'n', normal_completion_prefix .. 'r', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(0, 'n', normal_completion_prefix .. 's', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(0, 'n', normal_completion_prefix .. 't', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(0, 'n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
+  local opts_with_desc = function(desc)
+    return {
+      noremap = true,
+      silent = true,
+      buffer = bufnr,
+      desc = desc,
+    }
+  end
 
-  if client.server_capabilities.document_formatting then
-    vim.api.nvim_buf_set_keymap(0, 'n', normal_completion_prefix .. 'f', '<cmd>lua vim.lsp.buf.format { async = true }<CR>', {noremap = true})
-  elseif client.server_capabilities.document_range_formatting then
-    vim.api.nvim_buf_set_keymap(0, 'v', visual_completion_prefix .. 'f', '<cmd>lua vim.lsp.buf.range_formatting()<CR>', {noremap = true})
+  vim.keymap.set('n', '<c-]>', vim.lsp.buf.definition,      opts_with_desc('vim.lsp.buf.definition'))
+  vim.keymap.set('n', 'K',     vim.lsp.buf.hover,           opts_with_desc('vim.lsp.buf.hover'))
+  vim.keymap.set('n', 'yca',   vim.lsp.buf.code_action,     opts_with_desc('vim.lsp.buf.code_action'))
+  vim.keymap.set('n', 'ycd',   vim.lsp.buf.definition,      opts_with_desc('vim.lsp.buf.definition'))
+  vim.keymap.set('n', 'yci',   vim.lsp.buf.incoming_calls,  opts_with_desc('vim.lsp.buf.incoming_calls'))
+  vim.keymap.set('n', 'ycm',   vim.lsp.buf.implementation,  opts_with_desc('vim.lsp.buf.implementation'))
+  vim.keymap.set('n', 'ycn',   vim.lsp.buf.rename,          opts_with_desc('vim.lsp.buf.rename'))
+  vim.keymap.set('n', 'yco',   vim.lsp.buf.outgoing_calls,  opts_with_desc('vim.lsp.buf.outgoing_calls'))
+  vim.keymap.set('n', 'ycr',   vim.lsp.buf.references,      opts_with_desc('vim.lsp.buf.references'))
+  vim.keymap.set('n', 'ycs',   vim.lsp.buf.signature_help,  opts_with_desc('vim.lsp.buf.signature_help'))
+  vim.keymap.set('n', 'yct',   vim.lsp.buf.type_definition, opts_with_desc('vim.lsp.buf.type_definition'))
+  vim.keymap.set('n', 'ycl',   vim.lsp.codelens.run,        opts_with_desc('vim.lsp.codelens.run'))
+  vim.keymap.set('i', '<C-q>', vim.lsp.buf.signature_help,  opts_with_desc('vim.lsp.buf.signature_help'))
+
+  if client.server_capabilities.documentHighlightProvider then
+    local augroup_id = vim.api.nvim_create_augroup('DocumentHighlightGroup', { clear = false })
+    vim.api.nvim_clear_autocmds({ group = augroup_id, buffer = bufnr })
+
+    vim.api.nvim_create_autocmd(
+      { 'CursorHold' },
+      {
+        group = augroup_id,
+        buffer = bufnr,
+        callback = function()
+          vim.lsp.buf.clear_references()
+          vim.lsp.buf.document_highlight()
+        end
+      }
+    )
+  end
+
+  if client.server_capabilities.codeLensProvider then
+    local augroup_id = vim.api.nvim_create_augroup('CodeLensGroup', { clear = false })
+    vim.api.nvim_clear_autocmds({ group = augroup_id, buffer = bufnr })
+
+    vim.api.nvim_create_autocmd(
+      { 'BufEnter' },
+      {
+        group = augroup_id,
+        buffer = bufnr,
+        once = true,
+        callback = vim.lsp.codelens.refresh,
+      }
+    )
+
+    vim.api.nvim_create_autocmd(
+      { 'BufWritePost', 'CursorHold', 'InsertLeave' },
+      {
+        group = augroup_id,
+        buffer = bufnr,
+        callback = vim.lsp.codelens.refresh,
+      }
+    )
+  end
+
+  if client.server_capabilities.documentFormattingProvider then
+    local augroup_id = vim.api.nvim_create_augroup('DocumentFormattingGroup', { clear = false })
+    vim.api.nvim_clear_autocmds({ group = augroup_id, buffer = bufnr })
+
+    vim.api.nvim_create_autocmd(
+      { 'BufWritePre' },
+      {
+        group = augroup_id,
+        buffer = bufnr,
+        callback = function()
+          vim.lsp.buf.formatting_sync(nil, 1000)
+        end,
+      }
+    )
   end
 end
 
-vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
-vim.lsp.diagnostic.on_publish_diagnostics, {
-  -- delay update diagnostics.
-  update_in_insert = false,
-}
+vim.lsp.handlers['textDocument/publishDiagnostics'] = vim.lsp.with(
+  vim.lsp.diagnostic.on_publish_diagnostics, {
+    -- Delay update agnostics.
+    update_in_insert = false,
+  }
 )
 
-local servers = { 'pyright', 'tsserver', 'vimls'}
-for _, lsp in ipairs(servers) do
-  require('lspconfig')[lsp].setup {
-    on_attach = on_attach,
-    flags = {
-      debounce_text_changes = 150,
-    }
+base_server_settings = {
+  on_attach = base_custom_lsp_attach,
+  flags = {
+    -- TODO: Remove later, this will be the default in Neovim 0.7+.
+    debounce_text_changes = 150,
   }
+}
+
+local angularls_path = '/usr/local/lib/node_modules/@angular/language-server'
+local angularls_cmd = { 'ngserver', '--stdio', '--tsProbeLocations', angularls_path, '--ngProbeLocations', angularls_path }
+
+local servers = {
+  angularls = {
+    filetypes = { 'html', 'typescript' },
+    cmd = angularls_cmd,
+    on_new_config = function(new_config, new_root_dir)
+      new_config.cmd = angularls_cmd
+    end,
+  },
+
+  pyright = {
+    filetypes = { 'python' },
+  },
+
+  tailwindcss = {
+    filetypes = { 'html' },
+  },
+
+  tsserver = {
+    filetypes = { 'javascript', 'typescript' },
+    init_options = {
+      preferences = {
+        disableSuggestions = true,
+      },
+    },
+  },
+
+  gopls = {
+    filetypes = { 'go', 'gomod', 'gowork', 'gotmpl' },
+  },
+}
+
+for server, additional_server_settings in pairs(servers) do
+  local server_settings = vim.tbl_extend('force', base_server_settings, additional_server_settings)
+  lspconfig[server].setup(server_settings)
 end
