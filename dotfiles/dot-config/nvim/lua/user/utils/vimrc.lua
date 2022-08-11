8be4d470c3aefd59b63e3cd5da2f32247bca0829
Sam Ellis <samwilliamellis@gmail.com>
2022-08-11 12:48:10 +0100

    update: change local directory when calling `:Vimrc`

--- dotfiles/dot-config/nvim/lua/user/utils/vimrc.lua
+++ dotfiles/dot-config/nvim/lua/user/utils/vimrc.lua
@@ -1,1 +1,12 @@
-vim.api.nvim_create_user_command('Vimrc', ':e $MYVIMRC', {})
+if vim.fn.has('nvim-0.7.0') ~= 1 then
+  return
+end
+
+vim.api.nvim_create_user_command(
+  'Vimrc',
+  function()
+    vim.cmd.edit('$MYVIMRC')
+    vim.cmd.lcd(vim.fn.expand('%:.:h'))
+  end,
+  {}
+)
