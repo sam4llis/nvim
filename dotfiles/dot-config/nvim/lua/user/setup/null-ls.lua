2e9f8fc83ca76dc283afcf4db7087b66cedf9451
Sam Ellis <samwilliamellis@gmail.com>
2022-08-12 10:38:26 +0100

    update: format files with `null-ls` formatters on write

--- dotfiles/dot-config/nvim/lua/user/setup/null-ls.lua
+++ dotfiles/dot-config/nvim/lua/user/setup/null-ls.lua
@@ -10,4 +10,25 @@ local sources = {
   null_ls.builtins.formatting.blue,
 }
 
-null_ls.setup({ sources = sources })
+null_ls.setup({
+  sources = sources,
+  on_attach = function(client, bufnr)
+
+    if client.server_capabilities.documentFormattingProvider then
+      local augroup_id = vim.api.nvim_create_augroup('LspFormattingGroup', { clear = false })
+      vim.api.nvim_clear_autocmds({ group = augroup_id, buffer = bufnr })
+    end
+
+    vim.api.nvim_create_autocmd(
+      { 'BufWritePre' },
+      {
+        group = augroup_id,
+        buffer = bufnr,
+        callback = function()
+          vim.lsp.buf.formatting_sync(nil, 1000)
+        end,
+      }
+    )
+
+  end
+})
