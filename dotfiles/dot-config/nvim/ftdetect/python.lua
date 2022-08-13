3af21046196147f4296a3f80f913c41ac1a0e4a7
Sam Ellis <samwilliamellis@gmail.com>
2022-08-13 20:33:27 +0100

    feat: add python template
    I had these in my Vim (__not__ Neovim) configuration but forgot to port
    them over at the time. I'll start using more Python in my job soon so
    thought I'd prepare by adding these into my Neovim configuration.

---
+++ dotfiles/dot-config/nvim/ftdetect/python.lua
@@ -0,0 +1,13 @@
+local augroup_id = vim.api.nvim_create_augroup('Templates', { clear = false })
+vim.api.nvim_clear_autocmds({ group = augroup_id })
+
+vim.api.nvim_create_autocmd(
+  { 'BufNewFile' },
+  {
+    group = augroup_id,
+    pattern = { '*.py' },
+    callback = function()
+      vim.cmd('silent 0r ' .. vim.fn.expand('$HOME/.config/nvim/template/skeleton.py'))
+    end,
+  }
+)
