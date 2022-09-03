a6c8dd7f16595a1a9d60b84aeb8092c37073141d
Sam Ellis <samwilliamellis@gmail.com>
2022-09-03 15:13:58 +0100

    feat: add `setup()` for nvim-tundra

--- dotfiles/dot-config/nvim/lua/user/ui/colorscheme.lua
+++ dotfiles/dot-config/nvim/lua/user/ui/colorscheme.lua
@@ -5,8 +5,44 @@
 --   \___\___/|_|\___/|_|  |___/\___|_| |_|\___|_| |_| |_|\___(_)_|\__,_|\__,_|
 
 if vim.fn.isdirectory(vim.fn.expand('$HOME/.config/nvim/plugged/nvim-tundra')) == 1 and vim.fn.has('nvim-0.7.0') == 1 then
+  require('nvim-tundra').setup({
+    transparent_background = false,
+    editor = {
+      search = {},
+      substitute = {},
+    },
+    syntax = {
+      booleans = { bold = true, italic = true },
+      comments = { bold = true, italic = true },
+      conditionals = {},
+      constants = { bold = true },
+      functions = {},
+      keywords = {},
+      loops = {},
+      numbers = { bold = true },
+      operators = { bold = true },
+      punctuation = {},
+      strings = {},
+      types = { italic = true },
+    },
+    diagnostics = {
+      errors = {},
+      warnings = {},
+      information = {},
+      hints = {},
+    },
+    plugins = {
+      lsp = true,
+      treesitter = true,
+      cmp = true,
+      context = true,
+      gitsigns = true,
+      telescope = true,
+    },
+  })
+
   vim.opt.background = 'dark'
-  require('nvim-tundra').setup()
+  vim.cmd('colorscheme tundra')
   return
 end
 
