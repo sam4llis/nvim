4c8ecb4d5f175a93f9e526b942d2bbc718cb8330
Sam Ellis <samwilliamellis@gmail.com>
2022-10-16 13:48:26 +0100

    chore: change `cmp` deprecated method

--- dotfiles/dot-config/nvim/lua/user/setup/cmp.lua
+++ dotfiles/dot-config/nvim/lua/user/setup/cmp.lua
@@ -63,7 +63,7 @@ vim.api.nvim_create_autocmd('FileType', {
 -- TODO: Populate servers.
 local servers = {}
 
-local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
 for _, server in ipairs(servers) do
   require('lspconfig')[server].setup({
     capabilities = capabilities,
