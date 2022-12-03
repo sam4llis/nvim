216fe5b142ece92bfe339d3e410680931f75c9bf
Sam Ellis <samwilliamellis@gmail.com>
2022-12-03 18:15:58 +0000

    update: `:Run` command improvements
    Still a WIP, but might package this as a small plugin in the future. It
    would be interesting to be able to move the cursor back to the 'ran'
    window if the terminal is exited.

--- dotfiles/dot-config/nvim/after/ftplugin/python/keys.lua
+++ dotfiles/dot-config/nvim/after/ftplugin/python/keys.lua
@@ -6,8 +6,17 @@ vim.api.nvim_buf_create_user_command(
   'Run',
   function()
     vim.cmd('noautocmd write')
-    vim.cmd [[20split +terminal\ python3\ %]]
-    -- vim.cmd('startinsert')
+
+    vim.cmd.split()
+    local winnr = vim.api.nvim_get_current_win()
+
+    vim.cmd.wincmd('J')
+    vim.api.nvim_win_set_height(winnr, 12)
+
+    vim.opt_local.winfixheight = true
+    vim.opt_local.winfixwidth = true
+
+    vim.cmd.terminal('python3 %')
   end,
   {}
 )
