3dee4d322361638d857adea184f09b3df2ddd121
Sam Ellis <samwilliamellis@gmail.com>
2023-01-04 18:04:17 +0000

    chore: update fold preview

--- dotfiles/dot-config/nvim/plugin/foldtext.lua
+++ dotfiles/dot-config/nvim/plugin/foldtext.lua
@@ -1,5 +1,5 @@
 function _G.user_foldtext()
   local foldstart = vim.fn.getline(vim.v.foldstart)
   local preview = vim.fn.getline(vim.v.foldstart + 1):match('^%s*(.*)')
-  return foldstart .. preview .. ' ⋯'
+  return foldstart .. preview .. '"""'
 end
